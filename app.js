8a129807952094b7baab11c7fcf3691442e3dc3e
Arnesh Jairath <arnesh.jairath@hotmail.com>
2022-09-24 14:36:08 -0400

    refactored code and implemented json middleware

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-var express = require('express'),
-    user    = require('./routes/user'),
-    conversation = require('./routes/conversation');
+const express = require('express');
+const user = require('./routes/user');
+const conversation = require('./routes/conversation');
 
-const app = express()
-const port = 3000
+const app = express();
+app.use(express.json());
+
+const port = 3000;
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const options = {
-    definition: {
-      servers: [
-        {
-          url: `${process.env.URL}`.concat(":", port),
-        },
-      ],
-    },
-    apis: ["./routes/*.js"],
-  };
+  definition: {
+    servers: [{
+      url: `${process.env.URL}`.concat(":", port),
+    }, ],
+  },
+  apis: ["./routes/*.js"],
+};
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
-app.use('/user',  user);
-app.use('/conversation',  conversation);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  explorer: true
+}));
+app.use('/user', user);
+app.use('/conversation', conversation);
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
+  require('dotenv').config();
+}
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
