6809d11aa86c97aa05cfacb0a67bde3f0ef79eb8
Varghese Rony George <varghese.rony.george@gmail.com>
2022-10-01 15:00:06 -0400

    Prettier Format

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # chatter-backend
+
 A messenger/chat application server built on Node.JS
 
 ## Local Development Set Up
+
 After having Node.js and NPM installed, run `npm install` on the base folder to get the dependencies of this proejct.
 
 Install Docker as the local environment requires a mongoDB container for data. Upon installing, run `docker-compose up -d` in the base folder to run this instace.
 
-After this, run `npm start` to find the local server on __localhost:3000__
+After this, run `npm start` to find the local server on **localhost:3000**
 
 ## API Documentation
-Go to the base URL of the server, and use the route __/api-docs__.
 
-In the case of a local environment, this would be __localhost:3000/api-docs__
+Go to the base URL of the server, and use the route **/api-docs**.
 
+In the case of a local environment, this would be **localhost:3000/api-docs**
--- routes/conversation.js
+++ routes/conversation.js
@@ -1,28 +1,22 @@
-const express = require('express');
-const verifyToken = require('../utils/verifyToken');
+const express = require('express')
+const verifyToken = require('../utils/verifyToken')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/', function(req, res) {
-  if (!req.headers.authorization) {
-    return res.status(400).json({
-      message: "Invalid request"
-    });
-  };
-  
-  const userID = verifyToken(req.headers.authorization);
+router.post('/', function (req, res) {
+    if (!req.headers.authorization) {
+        return res.status(400).json({
+            message: 'Invalid request',
+        })
+    }
 
-  if (!userID) {
-    return res.status(440).json({
-      message: "Invalid login"
-    });
-  };
+    const userID = verifyToken(req.headers.authorization)
 
-  res.send('GET handler for /conversation route.');
-});
+    if (!userID) {
+        return res.status(440).json({
+            message: 'Invalid Credentials',
+        })
+    }
+})
 
-router.post('/', function(req, res) {
-  res.send('POST handler for /conversation route.');
-});
-
-module.exports = router;
+module.exports = router
