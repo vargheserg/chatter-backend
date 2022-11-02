971d0b40fcf62baf6122d46dad31f3a7a21c146c
Varghese Rony George <varghese.rony.george@gmail.com>
2022-11-02 19:37:14 -0400

    Update conversation.js

--- routes/conversation.js
+++ routes/conversation.js
@@ -149,14 +149,11 @@ router.get("/messages/:conversationId/", async function (req, res) {
           message: "Conversation does not exist",
         });
       }
-    
-    
-    
-    
+
       return res.status(200).json(conversation);
 });
 
-router.get("/:conversationId/", async function (req, res) {
+router.get("/:conversationId", async function (req, res) {
     if (!req.headers.authorization) {
         return res.status(400).json({
             message: "Invalid request",
@@ -170,49 +167,13 @@ router.get("/:conversationId/", async function (req, res) {
         });
     }
 
-    const conversation = await Conversation.find( {_id: req.path.conversationId});
+    const conversation = await Conversation.findById(new ObjectId(req.params.conversationId));
     if (conversation == null) {
         return res.status(404).json({
             message: "Conversation does not exist",
         });
     }
 
-    return res.status(200).json({
-        ...conversation._doc
-    });
-});
-
-router.get("/messages/:conversationId/", async function (req, res) {
-    if (!req.headers.authorization) {
-        return res.status(400).json({
-            message: "Invalid request",
-        });
-    }
-    var timeSentBefore = req.body.timeSentBefore;
-    if(timeSentBefore == null) {
-        timeSentBefore = new Date();
-    }
-
-    const userID = verifyToken(req.headers.authorization);
-    if (!userID) {
-        return res.status(440).json({
-            message: "Invalid Credentials",
-        });
-    }
-
-    const conversation = await Conversation.find( {_id: req.path.conversationId, messages: {timeSent: { $lte: timeSentBefore }}})
-    .limit( 10 )
-    .sort( '-timeSent' );
-
-    if (conversation == null) {
-        return res.status(404).json({
-            message: "Conversation does not exist",
-        });
-    }
-
-    
-
-    
     return res.status(200).json({
         ...conversation._doc
     });
